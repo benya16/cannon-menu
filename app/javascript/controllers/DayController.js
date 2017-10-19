8b4ca6e1f3b2013a5267b2e032a61eb6baf09477
Benjamin Carlisle <bjamin.carlisle@gmail.com>
2017-10-19 17:27:59 -0600

    Day controller works for today

--- app/javascript/controllers/DayController.js
+++ app/javascript/controllers/DayController.js
@@ -5,6 +5,25 @@
     cannonMenuService.getDailyMenu().then(function (result) {
         self.meals = result;
         console.log(result);
+
+
+        self.menus = [];
+
+        for(var i = 0; i < self.meals.length; ++i) {
+            self.menus[i] = self.meals[i].menus[0].recipes.filter(function (recipe) {
+
+                if(recipe.category === 'Euro' || recipe.category === 'Expo' || recipe.category === 'Grill' || recipe.category === 'Fusion') {
+                    return true;
+                }
+
+                return false;
+            });
+            self.menus[i].meal = self.meals[i].meal;
+        }
+
+
+        console.log("Menus: ", self.menus);
+
     });
 
 
